80c6fd826f53304b2226981d40c480f89cad3e87
Teddy Beau <Teddy@MacBook-Pro-Teddy.local>
2021-01-31 13:43:53 +0100

    added favicon and contact form

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+   console.log("document loaded");
+
+   // OPEN/CLOSE FORM
+   document.querySelector("#log-in-button").addEventListener("click", () => {
+      document.querySelector(".form-container").classList.remove("display");
+   });
+   document.querySelector("#close-form").addEventListener("click", () => {
+      document.querySelector(".form-container").classList.add("display");
+   });
+
+   // PROCESSING FORM SUBMISSIONS
+   document
+      .querySelector("#contact-form")
+      .addEventListener("submit", async (eventToListenTo) => {
+         eventToListenTo.preventDefault();
+         // Create an object to receive form's data
+         const data = {
+            firstname: document.querySelector("#firstname").value,
+            lastname: document.querySelector("#lastname").value,
+            email: document.querySelector("#email").value,
+            subject: document.querySelector("#subject").value,
+            message: document.querySelector("#message").value,
+         };
+         console.log("this is the data object: ", data);
+         // HTTP POST request to send the data to the backend
+         const response = await axios.post("http://localhost:3000", data);
+         console.log("this is the response from axios: ", response);
+      });
+});
